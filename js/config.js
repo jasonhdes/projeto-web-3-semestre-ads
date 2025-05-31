9df7d82f3bddc1912febd2b2821468404a1114d1
Jason Henrique de Souza <jhdes@hotmail.com>
2025-05-31 15:52:36 -0300

    layout base do projeto - página inicial

---
+++ js/config.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $('.coments').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        fade: true,
+        speed: 500
+    });
+
+    // $('.galeria').slick({
+    //     slidesToShow: 3,
+    //     slidesToScroll: 1,
+    //     autoplay: true,
+    //     autoplaySpeed: 1000,
+    // });
+});
